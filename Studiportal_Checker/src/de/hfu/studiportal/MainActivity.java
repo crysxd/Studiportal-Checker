050ede9a0aac9286bc3f169108e08cecec197cef
crysxd <c.wuerthner@me.com>
2014-07-18 13:22:10 +0200

    Revert "Removed refresh task starting in onCreate()"
    This reverts commit 8baf849b3d888381d2943b99d0d2022a91b53414.

--- Studiportal_Checker/src/de/hfu/studiportal/MainActivity.java
+++ Studiportal_Checker/src/de/hfu/studiportal/MainActivity.java
@@ -23,6 +23,9 @@ public class MainActivity extends DialogHostActivity {
 		getFragmentManager().beginTransaction().replace(android.R.id.content, 
 				new PreferencesFragment()).commit();
 
+		//Start Background Service
+		RefreshTaskStarter.startRefreshTask(this);
+
 	}
 
 	@Override

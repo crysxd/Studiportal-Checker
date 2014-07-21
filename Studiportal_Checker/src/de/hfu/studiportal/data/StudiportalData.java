4ac567e3182cbb4d98e2e37faf0c285fc5059467
crysxd <c.wuerthner@me.com>
2014-07-21 18:07:37 +0200

    Implemented StudiportalData
    StudiportalData can now parse the entire HTML table

--- Studiportal_Checker/src/de/hfu/studiportal/data/StudiportalData.java
+++ Studiportal_Checker/src/de/hfu/studiportal/data/StudiportalData.java
@@ -1,14 +1,152 @@
 package de.hfu.studiportal.data;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import android.content.SharedPreferences;
+import android.util.Base64;
 
 public class StudiportalData implements Serializable {
 
 	private static final long serialVersionUID = 5635830112259256921L;
 	private List<ExamCategory> categoryList = new ArrayList<>();
+
+	public static StudiportalData loadFromSharedPreferences(SharedPreferences sp, String key) throws Exception {
+		byte[] data = Base64.decode(sp.getString(key, "").getBytes(), Base64.DEFAULT);
+		ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(data));
+		StudiportalData sd = (StudiportalData) ois.readObject();
+		ois.close();
+		
+		return sd; 
+	}
 	
+	public StudiportalData() {
+		//Empty constructor needed to deserialize
+
+	}
+
+	public StudiportalData(String htmlTable) {
+		//Create Jsoup document
+		Document table = Jsoup.parse(htmlTable);
+
+		//Parse the table
+		this.parseTable(table);
+
+	}
+	
+	public void save(SharedPreferences sp, String key) throws IOException {
+		ByteArrayOutputStream bos = new ByteArrayOutputStream();
+		ObjectOutputStream oos = new ObjectOutputStream(bos);
+		oos.writeObject(this);
+		oos.close();
+		
+		sp.edit().putString(key, Base64.encodeToString(bos.toByteArray(), Base64.DEFAULT)).apply();
+	}
+
+	private void parseTable(Document table) {
+		//Get all rows
+		Elements rows = table.getElementsByTag("tr");
+
+		//var for current category
+		ExamCategory current = new ExamCategory("Unmatched");
+
+		//Iterate over rows
+		for (Element row : rows) {
+			try {
+				//If the rows contains a th element -> New category
+				int thCount = row.getElementsByTag("th").size();
+				if(thCount > 0) {
+					//More than 1x th? Must be the real header row -> skip
+					if(thCount > 1) 
+						continue;
+					
+					//Just one -> create new category
+					current = this.createExamCategory(row, current);
+					this.addCategory(current);
+
+				//Normal row, extract exam
+				} else {
+					current.addExam(this.createExam(row, current));
+
+				}
+			} catch(Exception e) {
+				//Do nothing
+
+			}
+		}
+	}
+
+	private Exam createExam(Element row, ExamCategory category) {
+		//Get all columns
+		Elements cols = row.getElementsByTag("td");
+		
+		//Create Exam
+		Exam e = new Exam(cols.get(0).text());
+		
+		//Extract data
+		int offset = 0;
+		for(int i=0; i<cols.size(); i++) {
+			//Save current col
+			Element col = cols.get(i);
+			
+			//If the col contains colspan attribute, skip this and all spanned columns
+			String colspan = col.attr("colspan");
+			if(colspan != null && colspan.length() > 0) {
+				int colspanInt = Integer.valueOf(col.attr("colspan"));
+				//Add the colspan to i (substract -1 because i++ on next iteration)
+				offset += colspanInt - 1;	
+				continue;
+				
+			}
+			
+			//Swicth case for every col
+			switch(i+offset) {
+			case 1: e.setName(col.text()); break;
+			case 2: e.setBonus(col.text()); break;
+			case 3: e.setMalus(col.text()); break;
+			case 4: e.setECTS(col.text()); break;
+			case 5: e.setSWS(col.text()); break;
+			case 6: e.setSemester(col.text()); break;
+			case 7: e.setKind(col.text()); break;
+			case 8: e.setTryCount(col.text()); break;
+			case 9: e.setGrade(col.text()); break;
+			case 10: e.setState(col.text()); break;
+			case 11: e.setComment(col.text()); break;
+			case 12: e.setResignation(col.text()); break;
+			case 13: e.setNote(col.text()); break;
+			}
+		}
+		
+		return e;
+	}
+
+	private ExamCategory createExamCategory(Element row, ExamCategory current) throws Exception {
+		//Extract name
+		String name = row.text();
+		
+		//If no name is set, skip this category, it's just a gap row (represented by Seperator)
+		if(name.length() == 0) {
+			current.addExam(new Seperator());
+			throw new RuntimeException();
+			
+		}
+		
+		//Create ExamCategory
+		return new ExamCategory(name);
+		
+	}
+
 	public void addCategory(ExamCategory e) {
 		this.categoryList.add(e);
 

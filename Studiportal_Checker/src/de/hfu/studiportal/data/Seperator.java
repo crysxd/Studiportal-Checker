3c5f6f7f88fe62162f59ebb6a2d42ef2e76a04d6
crysxd <c.wuerthner@me.com>
2014-07-21 17:48:12 +0200

    Added Seperator class

---
+++ Studiportal_Checker/src/de/hfu/studiportal/data/Seperator.java
@@ -0,0 +1,11 @@
+package de.hfu.studiportal.data;
+
+public class Seperator extends Exam {
+
+	private static final long serialVersionUID = 2180694412112484772L;
+
+	public Seperator() {
+		super(Seperator.class.getSimpleName());
+
+	}
+}

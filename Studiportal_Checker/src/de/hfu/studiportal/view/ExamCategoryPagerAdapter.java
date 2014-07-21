ab9a36f9e4989d5d0de214a25e582e813a4cb028
crysxd <c.wuerthner@me.com>
2014-07-21 23:32:14 +0200

    Refreshing when StudiportalData updated

--- Studiportal_Checker/src/de/hfu/studiportal/view/ExamCategoryPagerAdapter.java
+++ Studiportal_Checker/src/de/hfu/studiportal/view/ExamCategoryPagerAdapter.java
@@ -1,6 +1,8 @@
 package de.hfu.studiportal.view;
 
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
@@ -9,22 +11,18 @@ import android.support.v4.app.FragmentStatePagerAdapter;
 import de.hfu.funfpunktnull.R;
 import de.hfu.studiportal.data.StudiportalData;
 
-public class ExamCategoryPagerAdapter extends FragmentStatePagerAdapter {
+public class ExamCategoryPagerAdapter extends FragmentStatePagerAdapter implements OnSharedPreferenceChangeListener {
 	
 	private StudiportalData data;
+	private String studiportalDataKey;
 
 	public ExamCategoryPagerAdapter(FragmentManager fm, Context c) {
 		super(fm);
 		
-		try {
-			String key = c.getResources().getString(R.string.preference_last_studiportal_data);
-			data = StudiportalData.loadFromSharedPreferences(PreferenceManager.getDefaultSharedPreferences(c), key);
-			
-		} catch(Exception e) {
-			e.printStackTrace();
-			data = new StudiportalData();
-			
-		}
+		this.studiportalDataKey = c.getResources().getString(R.string.preference_last_studiportal_data);
+		PreferenceManager.getDefaultSharedPreferences(c).registerOnSharedPreferenceChangeListener(this);
+		this.loadDataAndRefresh(PreferenceManager.getDefaultSharedPreferences(c));
+
 	}
 
 	@Override
@@ -45,6 +43,30 @@ public class ExamCategoryPagerAdapter extends FragmentStatePagerAdapter {
 	@Override
 	public CharSequence getPageTitle(int position) {
 		return data.getCategory(position).getCategoryName();
+	}
+	
+	private void loadDataAndRefresh(SharedPreferences prefs) {
+		try {
+			data = StudiportalData.loadFromSharedPreferences(prefs, studiportalDataKey);
+			this.notifyDataSetChanged();
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+			
+			if(data == null)
+				data = new StudiportalData();
+			
+		}
+	}
+
+	@Override
+	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
+			String key) {
+		
+		if(!key.equals(studiportalDataKey))
+			return;
+		
+		this.loadDataAndRefresh(sharedPreferences);
 	}
 
 }

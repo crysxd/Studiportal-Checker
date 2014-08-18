c40b342c1fcea22e8e76bcc2e82271c78c64e6c1
crysxd <c.wuerthner@me.com>
2014-08-18 21:00:37 +0200

    Added FAB, arg to hide. Starting search on click

--- Studiportal_Checker/src/de/hfu/studiportal/view/ExamCategoryFragment.java
+++ Studiportal_Checker/src/de/hfu/studiportal/view/ExamCategoryFragment.java
@@ -1,18 +1,24 @@
 package de.hfu.studiportal.view;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import de.hfu.funfpunktnull.R;
 import de.hfu.studiportal.data.ExamCategory;
 
 //Instances of this class are fragments representing a single
 //object in our collection.
-public class ExamCategoryFragment extends ListFragment {
+public class ExamCategoryFragment extends ListFragment implements OnClickListener {
 	public static final String ARG_CATEGORY = "category";
+	public static final String ARG_HIDE_SEARCH_BUTTON = "hide_search";
 
+	private FloatingActionButton mSearchButton;
+	
 	@Override
 	public View onCreateView(LayoutInflater inflater,
 			ViewGroup container, Bundle savedInstanceState) {
@@ -25,7 +31,25 @@ public class ExamCategoryFragment extends ListFragment {
 		
 		//Set Adapter
 		this.setListAdapter(new ExamCategoryArrayAdapter(this.getActivity(), c.getAllExams()));
+		
+		//Attach Floating Button to ListView
+		ListView list = (ListView) rootView.findViewById(android.R.id.list);
+		mSearchButton = (FloatingActionButton) rootView.findViewById(R.id.button_floating_action);
+		mSearchButton.attachToListView(list);
+		mSearchButton.setOnClickListener(this);
+		
+		if(getArguments().getBoolean(ARG_HIDE_SEARCH_BUTTON)) {
+			mSearchButton.setVisibility(View.GONE);
 
+		}
+		
 		return rootView;
+	}
+
+	@Override
+	public void onClick(View v) {
+		Intent i = new Intent(this.getActivity(), ExamSearchActivity.class);
+		this.startActivity(i);
+				
 	}
 }

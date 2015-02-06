491e88ab4281562702678a10696c82453eb6978f
crysxd <c.wuerthner@me.com>
2015-02-06 23:43:42 +0100

    Removed list attribute

--- Studiportal_Checker/app/src/main/java/de/hfu/studiportal/view/ExamCategoryFragment.java
+++ Studiportal_Checker/app/src/main/java/de/hfu/studiportal/view/ExamCategoryFragment.java
@@ -17,8 +17,6 @@ import de.hfu.studiportal.data.ExamCategory;
 public class ExamCategoryFragment extends Fragment {
 	public static final String ARG_CATEGORY = "category";
 
-    public RecyclerView list;
-
 	@Override
 	public View onCreateView(LayoutInflater inflater,
 			ViewGroup container, Bundle savedInstanceState) {
@@ -30,12 +28,12 @@ public class ExamCategoryFragment extends Fragment {
 		ExamCategory c = (ExamCategory) getArguments().get(ARG_CATEGORY);
 
         //Find list and init
-        this.list = (RecyclerView) rootView.findViewById(R.id.list);
-        this.list.setLayoutManager(new LinearLayoutManager(this.getActivity()));
-        this.list.setItemAnimator(new DefaultItemAnimator());
+        RecyclerView list = (RecyclerView) rootView.findViewById(R.id.list);
+        list.setLayoutManager(new LinearLayoutManager(this.getActivity()));
+        list.setItemAnimator(new DefaultItemAnimator());
 
 		//Set Adapter
-		this.list.setAdapter(new ExamCategoryAdapter(this.getActivity(), c.getAllExams()));
+		list.setAdapter(new ExamCategoryAdapter(this.getActivity(), c.getAllExams()));
 		
 		return rootView;
 	}

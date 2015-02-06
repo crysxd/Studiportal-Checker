b9b5d891980c0dadf4eb17790cde62a8d7b5ae45
crysxd <c.wuerthner@me.com>
2015-02-06 22:00:06 +0100

    Updated PreferencesActivity

--- Studiportal_Checker/app/src/main/java/de/hfu/studiportal/view/PreferencesActivity.java
+++ Studiportal_Checker/app/src/main/java/de/hfu/studiportal/view/PreferencesActivity.java
@@ -1,6 +1,9 @@
 package de.hfu.studiportal.view;
 
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import de.hfu.funfpunktnull.R;
 
 /**
  * Empty Activity to hold the {@link PreferencesFragment}
@@ -15,8 +18,16 @@ public class PreferencesActivity extends DialogHostActivity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
+        //Build View
+        setContentView(R.layout.activity_preferences);
+
+        //Set up Toolbar
+        Toolbar bar = (Toolbar) findViewById(R.id.toolbar);
+        this.setSupportActionBar(bar);
+        this.getSupportActionBar().setTitle(this.getString(R.string.app_name));
+
 		// Load the preferences from an XML resource
-		getFragmentManager().beginTransaction().replace(android.R.id.content, 
+		getFragmentManager().beginTransaction().replace(R.id.preferences_fragment,
 				new PreferencesFragment()).commit();
 
 	}

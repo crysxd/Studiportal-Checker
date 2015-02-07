c32edb288e52de1cb84cc342eaf587e451415af5
crysxd <c.wuerthner@me.com>
2015-02-07 15:06:14 +0100

    Change PreferencesActivity title and enabled home button

--- Studiportal_Checker/app/src/main/java/de/hfu/studiportal/view/PreferencesActivity.java
+++ Studiportal_Checker/app/src/main/java/de/hfu/studiportal/view/PreferencesActivity.java
@@ -2,6 +2,7 @@ package de.hfu.studiportal.view;
 
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import de.hfu.funfpunktnull.R;
 
@@ -24,11 +25,24 @@ public class PreferencesActivity extends DialogHostActivity {
         //Set up Toolbar
         Toolbar bar = (Toolbar) findViewById(R.id.toolbar);
         this.setSupportActionBar(bar);
-        this.getSupportActionBar().setTitle(this.getString(R.string.app_name));
+        this.getSupportActionBar().setTitle(this.getString(R.string.text_activity_preferences_title));
+        this.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
 		// Load the preferences from an XML resource
 		getFragmentManager().beginTransaction().replace(R.id.preferences_fragment,
 				new PreferencesFragment()).commit();
 
 	}
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == android.R.id.home) {
+            this.finish();
+            return true;
+
+        }
+
+        return super.onOptionsItemSelected(item);
+
+    }
 }

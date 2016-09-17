dd2ea817b48cfa9cee42d797c8e43e4ffc919a25
Christian Würthner <christian@savedroid.de>
2016-09-17 22:07:27 +0200

    Revert "Added ad banner"
    This reverts commit 5be1f4a26da028620353aa67ed128914397a246a.

--- Studiportal_Checker/app/src/main/java/de/hfu/studiportal/view/ExamActivity.java
+++ Studiportal_Checker/app/src/main/java/de/hfu/studiportal/view/ExamActivity.java
@@ -1,24 +1,20 @@
 package de.hfu.studiportal.view;
 
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import de.hfu.funfpunktnull.R;
 import de.hfu.studiportal.data.Exam;
 
 public class ExamActivity extends DialogHostActivity {
-
+	
 	public static final String ARG_EXAM = "exam";
-
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
+		
 		//Set content View
 		this.setContentView(R.layout.activity_exam);
 
@@ -28,7 +24,7 @@ public class ExamActivity extends DialogHostActivity {
 
 		//Get arg
 		Exam e = (Exam) getIntent().getExtras().get(ExamActivity.ARG_EXAM);
-
+		
 		//Set Title
         this.getSupportActionBar().setTitle(e.getName());
 
@@ -37,14 +33,14 @@ public class ExamActivity extends DialogHostActivity {
 		if(e.getGrade() != null && e.getGrade().length() > 0) {
 			this.setText(e.getGrade(), R.id.textGrade);
 			this.setText(e.getStateName(this), R.id.textState);
-
+			
 		//If there is no grade
 		} else {
 			this.setText(e.getStateName(this), R.id.textGrade);
 			this.setText("", R.id.textState);
-
+			
 		}
-
+		
 		//Set icon
 		int d = 0;
 		switch (e.getStateEnum()) {
@@ -53,50 +49,17 @@ public class ExamActivity extends DialogHostActivity {
 		case NB: d = R.drawable.ic_nb; break;
 		case EN: d = R.drawable.ic_en; break;
 		default:
-
+			
 		}
-
-		// Set ad
-		ImageView ad = (ImageView) this.findViewById(R.id.imageViewAd);
-        String campain = "null";
-		if(e.getStateEnum() == Exam.State.BE || e.getStateEnum() == Exam.State.NB) {
-			if(e.getGrade().startsWith("1") || e.getGrade().startsWith("2")) {
-				ad.setImageResource(R.drawable.add_01);
-                campain = "ad_01";
-
-			} else if(e.getGrade().startsWith("3") || e.getGrade().startsWith("4")) {
-				ad.setImageResource(R.drawable.add_02);
-                campain = "ad_02";
-
-			} else {
-				ad.setImageResource(R.drawable.add_02);
-                campain = "ad_03";
-
-			}
-		} else {
-			ad.setImageDrawable(null);
-
-		}
-
-        final String campainFinal = campain;
-        ad.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(android.content.Intent.ACTION_VIEW);
-                i.setData(Uri.parse("https://play.google.com/store/apps/details?id=de.savedroid&hl=de&referrer=utm_source%3DStudiportal%26utm_medium%3DBanner%26utm_campaign%3D" + campainFinal));
-                startActivity(i);
-
-            }
-        });
-
+		
 		TextView tv = (TextView) findViewById(R.id.textState);
 		tv.setCompoundDrawablesWithIntrinsicBounds(d, 0, 0, 0);
 	}
-
+	
 	private void setText(String text, int id) {
 		TextView tv = (TextView) this.findViewById(id);
 		tv.setText(text);
-
+		
 	}
 
 }
